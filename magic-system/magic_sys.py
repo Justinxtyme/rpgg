2445b067c4f75f862cc8e2e09e98ca883e4460e3
Justinxtyme <binning.justin@gmail.com>
2025-03-29 03:40:57 -0700

    Update magic_sys.py

--- magic-system/magic_sys.py
+++ magic-system/magic_sys.py
@@ -1,40 +1,34 @@
 class Magic:
-    def __init__(self, name, magic_type,):
+    def __init__(self, name, magic_type, mp_cost, effect):
         self.name = name
         self.magic_type = magic_type
+        self.mp_cost = mp_cost
+        self.effect = effect
     
     def cast(self, caster, target):
+        
         
         
 
 class Divination(Magic):
     def __init__(self, name, magic_type, mp_cost, effect):
-        super().__init__(name, magic_type)
-        self.mp_cost = mp_cost
-        self.effect = effect
+        super().__init__(name, magic_type, mp_cost, effect)
+      
 class Sorcery(Magic):
     def __init__(self, name, magic_type, mp_cost, effect):
-        super().__init__(name, magic_type)
-        self.mp_cost = mp_cost
-        self.effect = effect
+        super().__init__(name, magic_type, mp_cost, effect)
 
 class Pyromancy(Magic):
     def __init__(self, name, magic_type, mp_cost, effect):
-        super().__init__(name, magic_type)
-        self.mp_cost = mp_cost
-        self.effect = effect
+        super().__init__(name, magic_type, mp_cost, effect)
 
 class BloodMagic(Magic):
     def __init__(self, name, magic_type, mp_cost, effect):
-        super().__init__(name, magic_type)
-        self.mp_cost = mp_cost
-        self.effect = effect
-
+        super().__init__(name, magic_type, mp_cost, effect)
+      
 class BladeMagic(Magic):
     def __init__(self, name, magic_type, mp_cost, effect):
-        super().__init__(name, magic_type)
-        self.mp_cost = mp_cost
-        self.effect = effect
+        super().__init__(name, magic_type, mp_cost, effect)
 
 minor_blessing = Divination(name="Minor Blessing", magic_type="Divination", mp_cost=20, effect=normal_heal)
 fire_blast = Pyromancy(name="Fire Blast", magic_type="Pyromancy", mp_cost=20, effect=fire_burst)

94452c5d1f72774a85466b290c79bb009e0142a0
Justinxtyme <binning.justin@gmail.com>
2025-03-29 04:11:20 -0700

    Update magic_sys.py

--- magic-system/magic_sys.py
+++ magic-system/magic_sys.py
@@ -39,4 +39,9 @@ class BladeMagic(Magic):
 
 minor_blessing = Divination(name="Minor Blessing", magic_type="Divination", mp_cost=20, effect=normal_heal)
 fire_blast = Pyromancy(name="Fire Blast", magic_type="Pyromancy", mp_cost=20, effect=fire_burst)
-blood_call = BloodMagix(name="Blood Call", magic_type ="Blood Magic",mp_cost=20, effect=
+blood_call = BloodMagix(name="Blood Call", magic_type ="Blood Magic",mp_cost=20, effect=None)
+
+
+spell_objects = {
+
+}

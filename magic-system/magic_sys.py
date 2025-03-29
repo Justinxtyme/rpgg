5c9edf347fe76e7506389b10d1814dc1622c6c8c
Justinxtyme <binning.justin@gmail.com>
2025-03-29 02:27:32 -0700

    Update magic_sys.py

--- magic-system/magic_sys.py
+++ magic-system/magic_sys.py
@@ -1,6 +1,7 @@
 class Magic:
     def __init__(self, name, magic_type,):
         self.name = name
+        self.magic_type = magic_type
 
 class Divination(Magic):
     def __init__(self, name, magic_type, mp_cost, effect):
@@ -20,6 +21,12 @@ class Pyromancy(Magic):
         self.effect = effect
 
 class BloodMagic(Magic):
+    def __init__(self, name, magic_type, mp_cost, effect):
+        super().__init__(name, magic_type)
+        self.mp_cost = mp_cost
+        self.effect = effect
+
+class BladeMagic(Magic):
     def __init__(self, name, magic_type, mp_cost, effect):
         super().__init__(name, magic_type)
         self.mp_cost = mp_cost

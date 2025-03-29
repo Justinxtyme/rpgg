c5472ab7edf391451ba4a9b5b74c21e734ffeb63
Justinxtyme <binning.justin@gmail.com>
2025-03-29 14:05:35 -0700

    Create push_subtrees.sh

---
+++ push_subtrees.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git subtree push --prefix=armor-system https://github.com/Justinxtyme/armor-system.git main
+git subtree push --prefix=character-system https://github.com/Justinxtyme/character-system.git main
+git subtree push --prefix=combat-system https://github.com/Justinxtyme/combat-system.git main
+git subtree push --prefix=effects-system https://github.com/Justinxtyme/effects-system.git main
+git subtree push --prefix=init https://github.com/Justinxtyme/init.git main
+git subtree push --prefix=items-system https://github.com/Justinxtyme/items-system.git main
+git subtree push --prefix=magic-system https://github.com/Justinxtyme/magic-system.git main
+git subtree push --prefix=weapons-system https://github.com/Justinxtyme/weapons-system.git main

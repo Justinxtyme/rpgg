925c5fd69415c603faeb51174fac236858a4438e
Justinxtyme <binning.justin@gmail.com>
2025-03-31 23:09:42 -0700

    Update magic.py

--- magic-system/magic.py
+++ magic-system/magic.py
@@ -24,6 +24,3 @@ class BloodMagic(Magic):
         super().__init__(name, magic_type)
         self.mp_cost = mp_cost
         self.effect = effect
-
-minor_blessing = Divination(name="Minor Blessing", magic_type="Divination", mp_cost=20, effect=normal_heal)
-fire_blast = Pyromancy(name="Fire Blast", magic_type="Pyromancy", mp_cost=20, effect=fire_burst)

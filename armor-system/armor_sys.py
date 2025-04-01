2ee2e4c71a29083a998883fc15b520f3e9eac041
Justinxtyme <binning.justin@gmail.com>
2025-03-31 22:28:48 -0700

    Update armor_sys.py

--- armor-system/armor_sys.py
+++ armor-system/armor_sys.py
@@ -13,12 +13,7 @@ class LightArmor(Armor):
     def __init__(self, name, weight, armor_type, armor_class, base_defense, rarity_mod, attribute_bonuses):
         super().__init__(name, weight, armor_type, armor_class, base_defense, rarity_mod, attribute_bonuses)
 
-leather_helm = Armor(name="Leather Helm", weight=2, armor_type="head", armor_class="light", base_defense=3, rarity_mod=1.0, attribute_bonuses=None)
-leather_cuirass = Armor(name="Leather Cuirass", weight=3, armor_type="chest", armor_class="light", base_defense=5, rarity_mod=1.0, attribute_bonuses=None)
-leather_boots = Armor(name="Leather Boots", weight=1, armor_type="feet", armor_class="light", base_defense=2, rarity_mod=1.0, attribute_bonuses=None)
-leather_leggings= Armor(name="Leather Leggings", weight=2, armor_type="legs", armor_class="light", base_defense=3, rarity_mod=1.0, attribute_bonuses=None)
-leather_vambraces = Armor(name="Leather Vambraces", weight=2, armor_type="arms", armor_class="light", base_defense=3, rarity_mod=1.0, attribute_bonuses=None)
-leather_belt = Armor(name="Leather Belt", weight=1, armor_type="waist", armor_class="light", base_defense=3, rarity_mod=1.0, attribute_bonuses=None)
+
 #equip tests
 Character.equip_armor(gandalf, leather_helm)
 Character.equip_armor(gandalf, leather_cuirass)

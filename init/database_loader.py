3d6e4993907b410ebb2861c05cdea7c0f51881ce
Justinxtyme <binning.justin@gmail.com>
2025-03-31 19:22:19 -0700

    Update database_loader.py

--- init/database_loader.py
+++ init/database_loader.py
@@ -10,7 +10,7 @@ WEAPONS = load_json("databases/weapons.json")
 ARMOR = load_json("databases/armor.json")
 ITEMS = load_json("databases/items.json")
 MAGIC = load_json("databases/magic.json")
-
+LOCATIONS = load_json("databases/locations.json")
 # Make sure this runs when imported
 if __name__ == "__main__":
     print("Databases loaded.")

8782ed10714ea5616f92ba3cffcb7b6f1829afb6
Justinxtyme <binning.justin@gmail.com>
2025-04-07 16:00:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 player = Character(0, 0)
+player_sprite = 
 
 while running:
     # poll for events
@@ -20,7 +21,8 @@ while running:
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
-
+    #uncomment for sprite
+    #screen.blit(player_sprite, player.position)
     pygame.draw.circle(screen, "red", player_pos, 40)
     keys = pygame.key.get_pressed()
     x_move = (keys[pygame.K_d] - keys[pygame.K_a])  # Right (1) / Left (-1)

c99c26ba367e128cdebbd070e068eabb20eaf56d
Justinxtyme <binning.justin@gmail.com>
2025-04-09 15:55:08 -0700

    Update database_loader.py

--- init/database_loader.py
+++ init/database_loader.py
@@ -7,18 +7,6 @@ def load_json(file_path):
         return json.load(f)
 
 
-
-def load_character_classes(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            character_classes = json.load(file)
-        return character_classes
-    except FileNotFoundError:
-        print(f"Error: File {file_path} not found.")
-        return {}
-    except json.JSONDecodeError:
-        print(f"Error: File {file_path} contains invalid JSON.")
-        return {}
 # Load databases
 CHARACTERS = load_json("databases/characters.json")
 WEAPONS = load_json("databases/weapons.json")

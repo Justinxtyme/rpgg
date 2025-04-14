8bcd7c4133a2e98f09157eae06d05ae99c70e2cd
Justinxtyme <binning.justin@gmail.com>
2025-04-14 16:39:55 -0700

    Update com_test.py

--- com_test.py
+++ com_test.py
@@ -1,3 +1,6 @@
+from char_system.char import Character
+from combat_system.combat import combat_loop
+
 #combat testing
 # Create your character instance (Gandalf) from your character module.
 gandalf = Character("Gandalf", "Mage", 0, 0)

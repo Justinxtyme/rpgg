0c134af30fe78d8b0df6164af5987d666f3f9751
Justinxtyme <binning.justin@gmail.com>
2025-04-14 16:38:30 -0700

    Update com_test.py

--- com_test.py
+++ com_test.py
@@ -1,1 +1,4 @@
 #combat testing
+# Create your character instance (Gandalf) from your character module.
+gandalf = Character("Gandalf", "Mage", 0, 0)
+buffneck = Character("BuffNeck", "Brute", 0, 0)

766b32dd5e090d808d01bbdc220babe493538f71
Justinxtyme <binning.justin@gmail.com>
2025-04-14 18:40:33 -0700

    Update player_actions.py

--- combat_system/player_actions.py
+++ combat_system/player_actions.py
@@ -1,5 +1,5 @@
 import magic_system.magic as magic
-#import combat_system.attacks as attacks
+import combat_system.attacks as attacks
 import items_system.items  # Ensure consumable logic is modular
 import combat_system.use_item as use_item
 from combat_system.zone import zone_menu

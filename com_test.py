2b64f6c13bc8f7394aa914e58b7dc9bd9f73ea11
Justinxtyme <binning.justin@gmail.com>
2025-04-14 17:09:54 -0700

    Update com_test.py

--- com_test.py
+++ com_test.py
@@ -1,11 +1,11 @@
 from char_system.char import Character
 from combat_system import combat
-import weapons_system.weapons
+import weapons_system.weapons as ws
 #combat testing
 # Create your character instance (Gandalf) from your character module.
 gandalf = Character("Gandalf", "Mage", 0, 0)
 buffneck = Character("BuffNeck", "Brute", 0, 0)
-bronze_dagger = weapons.Dagger(name="Bronze Dagger", weight=2, base_damage=8, rarity_mod=1.0, scaling={"dexterity": 1.0}, zone=None)
-bronze_straight_sword = weapons.MediumSword(name="Bronze Straight Sword", weight=4, base_damage=12, rarity_mod=1.0, scaling={"dexterity": 0.4, "strength": 0.6}, zone=None)
+bronze_dagger = ws.Dagger(name="Bronze Dagger", weight=2, base_damage=8, rarity_mod=1.0, scaling={"dexterity": 1.0}, zone=None)
+bronze_straight_sword = ws.MediumSword(name="Bronze Straight Sword", weight=4, base_damage=12, rarity_mod=1.0, scaling={"dexterity": 0.4, "strength": 0.6}, zone=None)
 
 combat.combat_loop(gandalf, buffneck, bronze_straight_sword, bronze_dagger)

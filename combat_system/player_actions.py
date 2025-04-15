1f1cea9b40ccb7c1b0897d4e28c621bfd8c1f9cb
Justinxtyme <binning.justin@gmail.com>
2025-04-14 17:00:31 -0700

    Update player_actions.py

--- combat_system/player_actions.py
+++ combat_system/player_actions.py
@@ -1,7 +1,6 @@
 import magic_system.magic
 import combat_system.attacks
 import items_system.items  # Ensure consumable logic is modular
-from combat_system.combat import zone_menu
 import combat_system.use_item
 #from equipment import swap_weapon  # Add weapon swapping function
 

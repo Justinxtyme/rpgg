4cbcd639a8d8984b0dc9b30eab34b2a77d2bf097
Justinxtyme <binning.justin@gmail.com>
2025-04-14 17:33:20 -0700

    Update com_test.py

--- com_test.py
+++ com_test.py
@@ -1,5 +1,5 @@
 from char_system.char import Character
-from combat_system import combat
+import combat_system.combat as combat
 import weapons_system.weapons as ws
 #combat testing
 # Create your character instance (Gandalf) from your character module.
